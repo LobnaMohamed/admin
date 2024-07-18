550f79c420d6e9249f40e6c4dc00ba575216df0e
Lobna Mahmoud <lobna@AMOC.COM>
2024-07-18 14:24:47 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// });
+Route::get('/dashboard',[DashboardController::class,'index']);
